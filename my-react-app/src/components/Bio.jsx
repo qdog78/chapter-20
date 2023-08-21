1601bb40f9a61b52a27f304653205d76c9a84842
qdog78 <qdog78@yahoo.com>
2023-08-21 18:33:48 -0500

    finished

--- my-react-app/src/components/Bio.jsx
+++ my-react-app/src/components/Bio.jsx
@@ -1,14 +1,18 @@
 function Bio() {
   return (
-    <p className="FlexCard1">
-      <div className="Font">
+    <p className="Font">
+       <img src="quin.jpeg" alt="Avatar" /> 
+      <div className="FlexCard1" >
         {" "}
         Born on the southside of Chicago, I studied at Western Illinois
         University. Finished with my degree in law enforcement, after getting a
         career in law enforcement and handling all of the stresses of being an
         LEO I decided to become a software developer. I hope you enjoy my
-        portfolio assignment
+        portfolio assignment!
       </div>
+      <div>
+
+</div>
     </p>
   );
 }
